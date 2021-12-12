a39f2605eb8c5b3fdcbba1410b22659f8f5b8b7a
ericdavidanderson <burrej1@gmail.com>
2021-12-12 13:20:00 -0800

    clean up

---
+++ utils/helper.js
@@ -0,0 +1,9 @@
+const dateHelper = {
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
+};
+
+module.exports = dateHelper;
